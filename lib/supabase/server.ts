373bd8ca2481263e2a2277647f00851c661779e5
Inno_z <phikanatekung@gmail.com>
2026-01-28 00:04:53 +0700

    แก้ไข server.ts และ client.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,7 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export async function createClient() {
   const cookieStore = await cookies()
